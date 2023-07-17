95ea7e70f1f65eeb98e91e6ffab02a94f16117f1
taleensa24-meet <139681215+taleensa24-meet@users.noreply.github.com>
2023-07-17 11:12:56 +0300

    Create pythonreview.py

---
+++ pythonreview.py
@@ -0,0 +1,23 @@
+def create_youtube_video(title,description):
+	ytvideo = {"title":title,"description": descriptipon,"likes":0,"dislikes" : 0, "comments":{"username": ""}}
+	return ytvideo
+
+
+def like(ytvideo):
+	if "like" in ytvideo:
+		ytvideo{"likes"}+=1
+		return ytvideo
+
+
+def dislike(ytvideo):
+	if "dislike" in ytvideo:
+		ytvideo{"dislikes"}+=1
+		return ytvideo
+
+
+def add_comment(ytvideo,username,comment_text):
+	comments["username"]=comment_text
+	return comments
+
+a_new_ytvideo=create_youtube_video{"new video","In this video i'll be taking u to a tour at iasa"}
+
